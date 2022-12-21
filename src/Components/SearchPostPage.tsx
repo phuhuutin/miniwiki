bba280a68c1210c71402df5628500a441c1153bf
phuhuutin <phuhuutin@gmail.com>
2022-12-21 00:43:22 -0800

    Search Page completed.

--- src/Components/SearchPostPage.tsx
+++ src/Components/SearchPostPage.tsx
@@ -7,11 +7,22 @@ export const SearchPostPage = () => {
   const [posts, setPosts] = useState<Post[]>();
   const [isLoading, setIsLoading] = useState(true);
   const [httpError, setHttpError] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const sizeOfEachPage: number = 3;
+  const [totalPages, setTotalPages] = useState(0);
+  const [totalElements, setTotalElements] = useState(0);
+  const [search, setSearch] = useState("");
+  const [searchUrl, setSearchUrl] = useState("");
 
   useEffect(() => {
     const fetchPosts = async () => {
       const baseUrl: string = "http://localhost:8081/api/posts";
-      const url: string = `${baseUrl}?page=0&size=9`;
+      let url: string = `${baseUrl}?page=${
+        currentPage - 1
+      }&size=${sizeOfEachPage}`;
+      if (searchUrl !== "") {
+        url = baseUrl + searchUrl;
+      }
       const response = await fetch(url);
       if (!response.ok) {
         throw new Error("Something went wrong");
@@ -21,6 +32,8 @@ export const SearchPostPage = () => {
       const responseData = responseJson._embedded.posts;
       const loadedPosts: Post[] = [];
       console.log(responseJson);
+      setTotalPages(responseJson.page.totalPages);
+      setTotalElements(responseJson.page.totalElements);
       for (const key in responseData) {
         loadedPosts.push({
           id: responseData[key].id,
@@ -40,7 +53,8 @@ export const SearchPostPage = () => {
       setIsLoading(false);
       setHttpError(error.message);
     });
-  }, []);
+    window.scrollTo(0, 0);
+  }, [currentPage, searchUrl]);
   if (isLoading) {
     return <SpinnerLoading />;
   }
@@ -52,6 +66,20 @@ export const SearchPostPage = () => {
       </div>
     );
   }
+
+  const paginate = (thepage: number) => {
+    setCurrentPage(thepage);
+  };
+
+  const searchHandleChange = () => {
+    if (search === "") {
+      setSearchUrl("");
+    } else {
+      setSearchUrl(
+        `/search/findByTitleContaining?title=${search}&page=0&size=${sizeOfEachPage}`
+      );
+    }
+  };
   return (
     <div className='container mx-auto '>
       <div className='grid gird-row-2'>
@@ -80,11 +108,16 @@ export const SearchPostPage = () => {
                 className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
                 placeholder='Search'
                 required
+                onChange={(e) => setSearch(e.target.value)}
               />
             </div>
             <button
               type='submit'
               className='p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+              onClick={() => {
+                searchHandleChange();
+                setCurrentPage(1);
+              }}
             >
               <svg
                 className='w-5 h-5'
@@ -105,16 +138,36 @@ export const SearchPostPage = () => {
           </form>
         </div>
       </div>
-      <div className=''>
-        <h5>Number of results: (22)</h5>
-      </div>
-      <p>1 to 5 of 22 items:</p>
+      {totalElements >= 1 ? (
+        <>
+          <div className=''>
+            <h5>Number of results: ({totalElements})</h5>
+          </div>
+          <p>
+            {sizeOfEachPage * currentPage - sizeOfEachPage + 1} to{" "}
+            {currentPage >= totalPages
+              ? totalElements
+              : sizeOfEachPage * currentPage}{" "}
+            of {totalElements} items:
+          </p>
+        </>
+      ) : (
+        <>
+          <div className=''>
+            <h5>Can't find what you are looking for! </h5>
+          </div>
+        </>
+      )}
       <div className='flex flex-col h-auto'>
         {posts?.map((post) => (
           <SearchPost key={post.id} post={post} />
         ))}
       </div>
-      <Pagination currentPage={1} totalPages={10} paginate={{}} />
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        paginate={paginate}
+      />
     </div>
   );
 };
